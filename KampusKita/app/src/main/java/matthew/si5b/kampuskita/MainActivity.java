ef741ec799633b6af1f5224c5906a4deb14cd15d
NathaciaLais <matthewvincentius01@gmail.com>
2022-11-10 12:54:13 +0700

    Fragment Bottom Navigation View

--- KampusKita/app/src/main/java/matthew/si5b/kampuskita/MainActivity.java
+++ KampusKita/app/src/main/java/matthew/si5b/kampuskita/MainActivity.java
@@ -1,14 +1,64 @@
 package matthew.si5b.kampuskita;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bnvkampuskita;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bnvkampuskita = findViewById(R.id.bnv_kampus_kita);
+
+        bukaFragment(new HomeFragment());
+        getSupportActionBar().setTitle("Home");
+
+        bnvkampuskita.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.menu_home:
+                        bukaFragment(new HomeFragment());
+                        getSupportActionBar().setTitle("Home");
+                        return true;
+                    case R.id.menu_jadwal:
+                        bukaFragment(new JadwalFragment());
+                        getSupportActionBar().setTitle("Jadwal");
+                        return true;
+                    case R.id.menu_nilai:
+                        bukaFragment(new NilaiFragment());
+                        getSupportActionBar().setTitle("Nilai");
+                        return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    private void bukaFragment(Fragment varFragment){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fl_container, varFragment);
+        ft.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_atas,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
