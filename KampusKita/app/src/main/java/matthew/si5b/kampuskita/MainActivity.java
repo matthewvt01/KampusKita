b2ca3ee4714ca4cc8af194cbec51a25c8fd24088
matthewvt01 <matthewvincentius01@gmail.com>
2022-11-10 12:04:13 +0700

    Fragment Bottom Navigation View

---
+++ KampusKita/app/src/main/java/matthew/si5b/kampuskita/MainActivity.java
@@ -0,0 +1,14 @@
+package matthew.si5b.kampuskita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
